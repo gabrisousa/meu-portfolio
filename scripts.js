96b699de12515242bd757883836b8d321e334400
Gabriel Sousa <104863987+gabrisousa@users.noreply.github.com>
2025-02-02 19:44:52 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector(".menu-bar");
+const perfil = document.querySelector(".caixa-perfil");
+const caixaContato = document.querySelector(".Caixa");
+
+
+function abrirBarra() {
+    perfil.style.display = "none"
+    caixaContato.style.display = "block"
+    
+
+}
+
+
+
+
+
+
