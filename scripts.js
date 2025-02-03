9543a7e1cdd9d33fcaf1f09c01c66c314d7a611a
Gabriel Sousa <104863987+gabrisousa@users.noreply.github.com>
2025-02-03 15:21:21 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,25 @@
 const menu = document.querySelector(".menu-bar");
+const icon = document.getElementById('icone1');
+const icon2 = document.getElementById('icone2');
 const perfil = document.querySelector(".caixa-perfil");
 const caixaContato = document.querySelector(".Caixa");
 
 
 function abrirBarra() {
+    icon.style.display = 'none';
+    icon2.style.display = 'block';
+    
     perfil.style.display = "none"
     caixaContato.style.display = "block"
     
+
+}
+
+function fecharBarra() {
+    icon2.style.display = 'none';
+    icon.style.display = 'block';
+    perfil.style.display = "block"
+    caixaContato.style.display = "none"
 
 }
 
